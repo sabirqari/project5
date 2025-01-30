d249a478f030d9d8d1dcde0dd5870e336176c7a7
nasrinna <nasrin.ahmad@noorteck.com>
2025-01-29 20:58:19 -0500

    a new code

--- src/Demo1/DemoTest5.java
+++ src/Demo1/DemoTest5.java
@@ -3,6 +3,12 @@ package Demo1;
 public class DemoTest5 {
 
 	public static void main(String[] args) {
-		System.out.println("Java");
+		System.out.println("Sabir");
+		
+		int a = 10 ;
+        int b = 20;
+        
+        System.out.println( a + b);
+        
 	}
 }
